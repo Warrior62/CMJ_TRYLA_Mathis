ad625821ee1fb14711eb40392554301184856fc9
Mathis <mathistryla@gmail.com>
2019-07-22 16:07:38 +0200

    add function addPlayer

--- players.js
+++ players.js
@@ -1,3 +1,18 @@
+//DECLARED VARIABLES//
+var playerAdded = document.getElementById('playerAdded');
+var i=1;
+
+//#playerAdded EMPTY//
+playerAdded.innerHTML='';
+
+
+//add a player in #playerAdded//
 function addPlayer(){
-    
+    i++;
+    playerAdded.innerHTML+='<label for=\'player'+i+'\'>'+'Player°'+i+':'+'</label>'+'<input type=\'text\' id=\'player'+i+'\'/ >'+'<br />';
+}
+
+function confirmPlayer(){
+    i=1;
+    playerAdded.innerHTML='';
 }
