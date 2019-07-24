dad207ba64c80d1e04e2e7e4f969d3300ad6f856
Mathis <mathistryla@gmail.com>
2019-07-24 22:06:52 +0200

    display results table when Confirm button is clicked

--- players.js
+++ players.js
@@ -1,5 +1,6 @@
 //DECLARED VARIABLES//
 var playerAdded = document.getElementById('playerAdded');
+var result = document.getElementById('result');
 
 //#playerAdded EMPTY//
 playerAdded.innerHTML='';
@@ -12,4 +13,19 @@ function addPlayer(){
 
 function confirmPlayer(){
     playerAdded.innerHTML='';
+    result.innerHTML='<table>'
+                    +   '<thead>'
+                    +       '<tr>'
+                    +           '<th colspan=\'2\'>Results</th>'
+                    +       '</tr>'
+                    +   '</thead>'
+                    +   '<tr>'
+                    +       '<td id=\'player1\'></td>'
+                    +       '<td>Computer</td>'
+                    +   '</tr>'
+                    +   '<tr>'
+                    +       '<td>24</td>'
+                    +       '<td>24</td>'
+                    +   '</tr>'
+                    +'</table>';
 }
